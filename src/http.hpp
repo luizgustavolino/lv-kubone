595e56596b65f6e5825d47b104301ef6c57ed954
Luiz Gustavo Lino <luizgustavolino@gmail.com>
2019-11-18 00:07:26 -0300

    functional snake game, withou lags

--- src/http.hpp
+++ src/http.hpp
@@ -12,11 +12,34 @@
 #include <fcntl.h>
 #include <signal.h>
 
+#define PAD_IDLE 	(unsigned short) 0
+#define PAD_DOWN 	(unsigned short) 1
+#define PAD_PRESSED (unsigned short) 2
+#define PAD_UP 		(unsigned short) 3
+
+struct Pad {
+	unsigned short up, down, left, right;
+	
+	Pad(){
+		clear();
+	}
+
+	void clear(){
+		up = down = left = right = 0;
+	}
+
+	void print(){
+		printf("pad state w:%d a:%d s:%d d:%d\n", up, left, down, right);
+	}
+};
+
 struct HTTP {
 
-	void (*onRequest)(int);
+	void (*onRequest)(HTTP*);
+	int clientfd;
+	Pad pad;
 
-	HTTP(const char *port, void (*_onRequest)(int)) {
+	HTTP(const char *port, void (*_onRequest)(HTTP*)) {
 		onRequest = _onRequest;
 		bindSocket(port);
 		acceptFoverer();
@@ -25,7 +48,6 @@ struct HTTP {
 private:
 
 	int listenfd;
-	int clientfd;
 	struct sockaddr_in clientaddr;
 	socklen_t addrlen;
 
@@ -52,18 +74,35 @@ private:
 		rcvd = recv(clientfd, msg, 99999, 0);
 
 		if (rcvd < 0) {
+
 			// receive error
 			fprintf(stderr,("recv() error\n"));
 
 		} else if (rcvd == 0) {
+
 			// receive socket closed
 			fprintf(stderr,"Client disconnected upexpectedly.\n");
 
 		} else {
+
 			// receive message
 			printf("%s\n", msg);
-			write(clientfd, "HTTP/1.0 200 OK\n\n", 17);
-			if(onRequest != NULL) onRequest(clientfd);
+
+			const char* hint = "wasd=\0";
+			char* location = strstr(msg, hint);
+
+			if (location != NULL ) {
+
+  				pad.up    = ((unsigned short) *(location + 5)) - 48;
+  				pad.left  = ((unsigned short) *(location + 6)) - 48;
+  				pad.down  = ((unsigned short) *(location + 7)) - 48;
+  				pad.right = ((unsigned short) *(location + 8)) - 48;
+
+  				write(clientfd, "HTTP/1.0 200 OK\n\n", 17);
+				if(onRequest != NULL) onRequest(this);
+			} else {
+				write(clientfd, "HTTP/1.0 400 BAD REQUEST\n\n", 17);
+			}
 		}
 
 		shutdown(clientfd, SHUT_RDWR);
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
 #include "bmp.hpp"
 #include "notify.hpp"
 #include "http.hpp"
@@ -6,50 +9,179 @@
 #define IN_PATH  "/var/sim/lv-kubone/runtime/inputmap.txt"
 #define OUT_PATH "/var/sim/lv-kubone/runtime/frame.bmp"
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 //Screen dimension constants
 const int SCREEN_WIDTH = 160;
 const int SCREEN_HEIGHT = 128;
 
-int frame = 0;
-int speedx = 1;
-int speedy = 1;
-int partSize = 10;
-int x = 5;
-int y = 20;
-BMP frame_buffer(SCREEN_WIDTH, SCREEN_HEIGHT);
+int  frame = 0;
+char direction;
+int  partSize = 10;
+int  dead  = 0;
+int  speed = 15;
 
 struct BodyPart {
     BodyPart *next;
-    int x;
-    int y;
+    unsigned short x;
+    unsigned short y;
+};
+
+struct Apple{
+    unsigned short x;
+    unsigned short y;
 };
 
 BodyPart *head = NULL;
+Apple *apple = NULL;
 
-void onEnter(){
+BMP frame_buffer(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+BodyPart* addBodyPart(BodyPart* after, unsigned short x, unsigned short y) {
     head = (BodyPart*) malloc(sizeof(BodyPart));
-    head->next = NULL;
-    head->x = 2;
-    head->y = 4;
+    head->next = after;
+    head->x = x;
+    head->y = y;
 }
 
-void nextFrame(int fd) {
+bool hasBodyAt(int x, int y, BodyPart* start = NULL) {
+    BodyPart *current = start == NULL ? head : start;
+    do {
+        if (current->x == x && current->y == y) return true;
+        else current = current->next;
+    } while (current != NULL);
+    return false;
+}
 
-    frame++;
-    frame_buffer.fill_region(0, 0, 160, 128, 0, 0, 0);
+void addRandomApple(){
+
+    if(apple != NULL){
+        free(apple);
+        apple = NULL;
+    }
+    
+    srand(frame); int x = rand()%10;
+    srand(frame); int y = rand()%10;
+
+    if (!hasBodyAt(x, y)) {
+        apple = (Apple*) malloc(sizeof(Apple));
+        apple->x = x;
+        apple->y = y;
+        speed --;
+    }
+}
+
+bool removeTail(){
 
     BodyPart *current = head;
+    BodyPart *before  = NULL;
 
-    do {
-        frame_buffer.fill_region(current->x * partSize + 1,current->y * partSize + 1,
-                                    partSize, partSize, 0x00, 0xC0, 0xDE);
+    while (current->next != NULL) {
+        before = current;
         current = current->next;
-    } while (current != NULL);
+    }
 
-    x += speedx;
-    y += speedy;
-    if (x <= 0 || x >= 150) speedx *= -1;
-    if (y <= 0 || y >= 118) speedy *= -1;
+    if(current != NULL && before != NULL){
+        before->next = NULL;
+        free(current);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void killSnake(){
+    do {} while (removeTail());
+    free(head);
+    head = NULL;
+}
+
+void onEnter(){
+    direction = 'a';
+    speed = 14;
+    head = addBodyPart(NULL, 2, 8);
+    BodyPart *body = addBodyPart(head, 2, 7);
+    BodyPart *tail = addBodyPart(body, 2, 6);
+    addRandomApple();
+}
+
+void nextFrame(HTTP *server) {
+
+    int fd = server->clientfd;
+    frame++;
+
+    if(dead > 0) {
+
+        double blend = MAX(0, MIN(1 , dead > 60 ? 0 : ((double)(30 - (dead-30))/30.0)));
+        frame_buffer.fill_region(0, 0, 11*10, 11*10,
+            0xC0 - ((int) (blend * (0xC0 - 0x22))),
+            0x33 - ((int) (blend * (0x33 - 0x22))),
+            0x33 - ((int) (blend * (0x33 - 0x22))));
+
+        if (dead > 60){
+            BodyPart *current = head;
+
+            do {
+                bool shaking = dead > 120 && dead < 160;
+                double blend = MAX(0, MIN(1 , dead > 90 ? 0 : ((double)(30 - (dead-60)) / 30.0)));
+                frame_buffer.fill_region( (shaking ? rand()%3 - 1 : 0) + current->x * (partSize + 1),
+                                          (shaking ? rand()%3 - 1 : 0) + current->y * (partSize + 1),
+                                          partSize, partSize,
+                                          0xFF - ((int) (blend * (0xFF - 0xC0))),
+                                          0xFF - ((int) (blend * (0xFF - 0x33))),
+                                          0xFF - ((int) (blend * (0xFF - 0x33))));
+                current = current->next;
+            } while (current != NULL);
+        }
+
+        dead--;
+
+        if(!dead) {
+            killSnake();
+            onEnter();
+        }
+
+    } else {
+
+        frame_buffer.fill_region(0, 0, 160, 128, 0x00, 0x00, 0x00);
+        frame_buffer.fill_region(0, 0, 11*10, 11*10, 0x22, 0x22, 0x22);
+        BodyPart *current = head;
+
+        do {
+            frame_buffer.fill_region( current->x * (partSize + 1), current->y * (partSize + 1),
+                                      partSize, partSize, 0x00 + current->y * 20, 0xC0, 0xDE - current->y * 20);
+            current = current->next;
+        } while (current != NULL);
+
+        if (apple != NULL) {
+            int glow = (int) (sin(((double)frame)/5.0) * 50);
+            frame_buffer.fill_region(11*apple->x + 1, 11*apple->y + 1, 8, 8, 0xDE, 60 + glow, 60 + glow);
+        }
+
+        if (server->pad.up    == PAD_DOWN && direction != 's') direction = 'w';
+        if (server->pad.left  == PAD_DOWN && direction != 'd') direction = 'a';
+        if (server->pad.down  == PAD_DOWN && direction != 'w') direction = 's';
+        if (server->pad.right == PAD_DOWN && direction != 'a') direction = 'd';
+
+        if (apple == NULL) addRandomApple();
+
+        if (frame % MAX(6, speed) == 0){
+
+            switch (direction) {
+                case 'w': head = addBodyPart(head, head->x, (head->y + 1)%10); break;
+                case 'd': head = addBodyPart(head, (head->x + 1)%10, head->y); break;
+                case 'a': head = addBodyPart(head, head->x > 0 ? head->x - 1 : 9, head->y); break;
+                case 's': head = addBodyPart(head, head->x, head->y > 0 ? head->y - 1 : 9); break;
+                default: break;
+            }
+
+            if (hasBodyAt(head->x, head->y, head->next)) dead = 180;
+            else if (apple == NULL) addRandomApple();
+            else if (head->x == apple->x && head->y == apple->y) addRandomApple();
+            else removeTail();
+        }
+    }
 
     frame_buffer.writeToFD(fd);
 }
